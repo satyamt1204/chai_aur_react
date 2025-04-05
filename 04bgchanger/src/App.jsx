58593c511b47bbece23dc54a118883a3fa40d352
satyamt1204 <satyamtek1204@gmail.com>
2025-04-05 12:59:33 +0530

    bg changer by hitesh

--- 04bgchanger/src/App.jsx
+++ 04bgchanger/src/App.jsx
@@ -1,7 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/* import { useState } from 'react'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,12 +10,36 @@ function App() {
     <>
       <div className={`w-full flex items-end justify-center min-h-screen p-6 ${color}`} >
         <div className='flex rounded-2xl bg-white px-6 py-3 gap-5 shadow-lg items-center'>
-          <button className='bg-red-800 rounded-4xl w-16 p-1 border-2 text-white hover:border-blue-500' onClick={changecolor.bind(null, "bg-red-800")}>Red</button>
-          <button className='bg-green-800 rounded-4xl w-16 p-1 border-2 text-white hover:border-blue-500' onClick={changecolor.bind(null, "bg-green-800")}>Green</button>
-          <button className='bg-blue-800 rounded-4xl w-16 p-1 border-2 text-white hover:border-blue-500' onClick={changecolor.bind(null, "bg-blue-800")}>Blue</button>
+          <button className='bg-red-800 rounded-4xl w-16 p-1 border-4 text-white hover:border-blue-500' onClick={changecolor.bind(null, "bg-red-800")}>Red</button>
+          <button className='bg-green-800 rounded-4xl w-16 p-1 border-4 text-white hover:border-blue-500' onClick={changecolor.bind(null, "bg-green-800")}>Green</button>
+          <button className='bg-blue-800 rounded-4xl w-16 p-1 border-4 text-white hover:border-blue-500' onClick={changecolor.bind(null, "bg-blue-800")}>Blue</button>
         </div>
       </div>
     </>
+  )
+}
+
+export default App
+ */
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+  let [color, setColor] = useState("olive")
+  const changecolor = (Color = "bg-blue-800") => {
+    setColor(Color);
+  }
+  return (
+    <div className='w-full h-screen duration-200' style={{backgroundColor : color}}>
+      <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
+        <div className='flex flex-wrap justify-center shadow-lg bg-white px-3 py-2 rounded-3xl gap-3
+        '>
+          <button className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style={{backgroundColor : "red"}} onClick={() => setColor("red")}>Red</button>
+          <button className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style={{backgroundColor : "green"}} onClick={() => setColor("green")}>Green</button>
+          <button className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style={{backgroundColor : "blue"}} onClick={() => setColor("blue")}>Blue</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
