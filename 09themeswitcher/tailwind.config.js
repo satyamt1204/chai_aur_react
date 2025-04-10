13cc76b0c21e7b44ca66a82131ac071ecafd2953
satyamt1204 <satyamtek1204@gmail.com>
2025-04-11 00:28:29 +0530

    context understood

---
+++ 09themeswitcher/tailwind.config.js
@@ -0,0 +1,9 @@
+{import('tailwindcss').Config}
+module.exports = {
+    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx,html}'],
+    darkMode: 'class',
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
